7dafa16ca44e6669dc3579e05b866914942cbce7
lindragom94-star <lindragom94@gmail.com>
2026-04-28 15:13:33 +0800

    Update s1121556-UVa673.cpp

--- On-Site_Examinations/Week10_01_UVa673/src/s1121556-UVa673.cpp
+++ On-Site_Examinations/Week10_01_UVa673/src/s1121556-UVa673.cpp
@@ -1,1 +1,60 @@
-
+#include <stack>
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+  stack<char> c;
+  string s;
+  char a;
+  bool flag;
+  int N;
+  cin>>N;
+  for(int i=1;i<=N;i++){
+    cin>>s;
+    for(int j=0;j<s.size();j++){
+      if(s[j]=='(' || s[j]=='['){
+        c.push(s[j]);
+      }
+      if(s==""){
+        flag=true;
+        break;
+      }
+      if(s[j]==")"){
+        if(c.empty()==true){
+          flag=false;
+          break;
+        }
+        if(c.top()=='('){
+          flag=true;
+          c.pop();
+        }
+        else{
+          flag=false;
+          c.pop();
+          break;
+        }
+      }
+      if(s[j]=="]"){
+        if(c.empty()==true){
+          flag=false;
+          break;
+        }
+        if(c.top()=='['){
+          flag=true;
+          c.pop();
+        }
+        else{
+          flag=false;
+          c.pop();
+          break;
+        }
+      }
+    }
+    if(flag==true){
+      cout<<"Yes"<<endl;
+    }
+    if(flag==false){
+      cout<<"No"<<endl;
+    }
+  }
+}
